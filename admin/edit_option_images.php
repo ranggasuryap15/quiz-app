1f39099e4140827003fa94c8b2b56bf309e7fc9d
ranggasuryap15 <ranggasurya.313@gmail.com>
2022-04-17 17:07:07 +0700

    memperbaiki edit option images dan delete option

--- admin/edit_option_images.php
+++ admin/edit_option_images.php
@@ -6,22 +6,22 @@ $id = $_GET["id"];
 $id1 = $_GET["id1"]; // ini untuk id yang exam_category
 
 $question = "";
-$opt1 = "";
-$opt2 = "";
-$opt3 = "";
-$opt4 = "";
-$answer = "";
+$option_1 = "";
+$option_2 = "";
+$option_3 = "";
+$option_4 = "";
+$answer_db = "";
 
 $res = mysqli_query($link, "select * from questions where id_question=$id");
 while ($row = mysqli_fetch_array($res))
 {
     // untuk mendapatkan value dari database dimasukkan ke dalam sebuah variabel
     $question = $row["question"];
-    $opt1 = $row["option_1"];
-    $opt2 = $row["option_2"];
-    $opt3 = $row["option_3"];
-    $opt4 = $row["option_4"];
-    $answer = $row["answer"];
+    $option_1 = $row["option_1"];
+    $option_2 = $row["option_2"];
+    $option_3 = $row["option_3"];
+    $option_4 = $row["option_4"];
+    $answer_db = $row["answer"];
 }
 ?>
 <div class="breadcrumbs">
@@ -54,31 +54,31 @@ while ($row = mysqli_fetch_array($res))
                                         </div>
                                         
                                         <div class="form-group">
-                                            <img src="<?php echo $opt1 ?>" height="50" width="50" alt="Option 1"><br>
+                                            <img src="<?php echo $option_1 ?>" height="50" width="50" alt="Option 1"><br>
                                             <label class=" form-control-label">Tambahkan Opsi 1</label>
                                             <input type="file" name="img_opt1" class="form-control">
                                         </div>
                                         
                                         <div class="form-group">
-                                            <img src="<?php echo $opt2 ?>" height="50" width="50" alt="Option 2"><br>
+                                            <img src="<?php echo $option_2 ?>" height="50" width="50" alt="Option 2"><br>
                                             <label class=" form-control-label">Tambahkan Opsi 2</label>
                                             <input type="file" name="img_opt2" class="form-control">
                                         </div>
 
                                         <div class="form-group">
-                                            <img src="<?php echo $opt3 ?>" height="50" width="50" alt="Option 3"><br>
+                                            <img src="<?php echo $option_3 ?>" height="50" width="50" alt="Option 3"><br>
                                             <label class=" form-control-label">Tambahkan Opsi 3</label>
                                             <input type="file" name="img_opt3" class="form-control">
                                         </div>
 
                                         <div class="form-group">
-                                            <img src="<?php echo $opt4 ?>" height="50" width="50" alt="Option 4"><br>
+                                            <img src="<?php echo $option_4 ?>" height="50" width="50" alt="Option 4"><br>
                                             <label class=" form-control-label">Tambahkan Opsi 4</label>
                                             <input type="file" name="img_opt4" class="form-control">
                                         </div>
 
                                         <div class="form-group">
-                                            <img src="<?php echo $answer ?>" height="50" width="50" alt="Answer"><br>
+                                            <img src="<?php echo $answer_db ?>" height="50" width="50" alt="Answer"><br>
                                             <label class=" form-control-label">Jawaban Benar</label> <br>
                                             <input type="file" name="img_answer_opt" class="form-control">
                                         </div>
@@ -111,6 +111,7 @@ if (isset($_POST["submit2"]))
     // untuk update pada pertanyaan dan option 1
     if ($opt1 != "")
     {
+        unlink($option_1);
         // untuk penamaan file dan direktori pada database (option image 1)
         $file_tmp_1 = $_FILES['img_opt1']['tmp_name'];
         $dst1 = "./option_images/" . $tm . $opt1;
@@ -123,6 +124,7 @@ if (isset($_POST["submit2"]))
     // untuk update pada pertanyaan dan option 2
     if ($opt2 != "")
     {
+        unlink($option_2);
         // untuk penamaan file dan direktori pada database (option image 2)
         $file_tmp_2 = $_FILES['img_opt2']['tmp_name'];
         $dst2 = "./option_images/" . $tm . $opt2;
@@ -135,37 +137,41 @@ if (isset($_POST["submit2"]))
     // untuk update pada pertanyaan dan option 3
     if ($opt3 != "")
     {
-        // untuk penamaan file dan direktori pada database (option image 3)
+        unlink($option_3);
+        // untuk penamaan file dan direktori pada database (option image 2)
         $file_tmp_3 = $_FILES['img_opt3']['tmp_name'];
         $dst3 = "./option_images/" . $tm . $opt3;
-        $file_destination_3 = "option_images/" . $tm . $opt3;
+        $file_destination_3 = "option_images/" . $tm . $opt3; // untuk database
         move_uploaded_file($file_tmp_3, $dst3);
-        
+
         mysqli_query($link, "update questions set question='$_POST[img_question]', option_3='$file_destination_3' where id_question=$id") or die(mysqli_error($link));
     }
 
     // untuk update pada pertanyaan dan option 4
     if ($opt4 != "")
     {
-        // untuk penamaan file dan direktori pada database (option image 4)
+        unlink($option_4);
+        // untuk penamaan file dan direktori pada database (option image 3)
         $file_tmp_4 = $_FILES['img_opt4']['tmp_name'];
         $dst4 = "./option_images/" . $tm . $opt4;
         $file_destination_4 = "option_images/" . $tm . $opt4;
-        move_uploaded_file($file_tmp_4, $dst4);  
-
+        move_uploaded_file($file_tmp_4, $dst4);
+        
         mysqli_query($link, "update questions set question='$_POST[img_question]', option_4='$file_destination_4' where id_question=$id") or die(mysqli_error($link));
     }
 
     // untuk update pada pertanyaan dan answer
     if ($answer != "")
     {
+        unlink($answer_db);
         // untuk penamaan file dan direktori pada database (option image 4)
         $file_tmp_5 = $_FILES['img_answer_opt']['tmp_name'];
-        $dst4 = "./option_images/" . $tm . $answer;
+        $dst5 = "./option_images/" . $tm . $answer;
         $file_destination_5 = "option_images/" . $tm . $answer;
         move_uploaded_file($file_tmp_5, $dst5);  
-
+        
         mysqli_query($link, "update questions set question='$_POST[img_question]', answer='$file_destination_5' where id_question=$id") or die(mysqli_error($link));
+        
     }
 
     // untuk update pertanyaan saja tanpa gambar yang lain
