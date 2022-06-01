4920087a60107b70dac3cbaa2ee76918a3fe997f
ranggasuryap15 <ranggasurya.313@gmail.com>
2022-06-02 00:30:17 +0700

    merapikan SQL

--- forajax/load_questions.php
+++ forajax/load_questions.php
@@ -19,7 +19,7 @@ if (isset($_SESSION["answer"][$que_no]))
     $ans = $_SESSION["answer"][$que_no];
 }
 
-$res = mysqli_query($link, "select * from questions where quiz_name='$_SESSION[quiz]' && question_no=$_GET[question_no]");
+$res = mysqli_query($link, "SELECT * FROM questions WHERE quiz_name='$_SESSION[quiz]' && question_no=$_GET[question_no]");
 $count = mysqli_num_rows($res);
 
 if ($count == 0) {
