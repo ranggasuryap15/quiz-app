a831036dce6ced6956d752110f68211e23e8dd54
ranggasuryap15 <ranggasurya.313@gmail.com>
2022-06-02 00:32:41 +0700

    mengganti SQL agar result.php dapat berjalan semestinya

--- forajax/set_exam_type_session.php
+++ forajax/set_exam_type_session.php
@@ -4,10 +4,11 @@ include "../connection.php";
 $quiz = $_GET["quiz"];
 $_SESSION["quiz"] = $quiz;
 
-$res=mysqli_query($link,"select * from quiz where quiz_name='$quiz'");
+$res = mysqli_query($link,"SELECT * FROM quiz WHERE quiz_name='$quiz'");
 while($row=mysqli_fetch_array($res))
 {
     $_SESSION["quiz_timer"] = $row["quiz_timer"];
+    $_SESSION["id_quiz"] = $row["id_quiz"];
 }
 date_default_timezone_set('Asia/Jakarta');
 $date = date("Y-m-d H:i:s");
--- result.php
+++ result.php
@@ -83,7 +83,7 @@ include_once "connection.php";
                 if(isset($_SESSION["answer"])){
                     for ($i = 1; $i <= sizeof($_SESSION["answer"]); $i++){
                         $answer="";
-                        $res = mysqli_query($link,"select * from questions where quiz_name='$_SESSION[quiz]' && question_no=$i");
+                        $res = mysqli_query($link,"SELECT * FROM questions WHERE quiz_name='$_SESSION[quiz]' && question_no=$i");
 
                         while($row=mysqli_fetch_array($res)){
                                 $answer = $row["answer"];
@@ -102,7 +102,7 @@ include_once "connection.php";
                 }
 
                 $total_question = 0;
-                $res = mysqli_query($link,"select * from questions where quiz_name='$_SESSION[quiz]'");
+                $res = mysqli_query($link,"SELECT * FROM questions WHERE quiz_name='$_SESSION[quiz]'");
                 $total_question = mysqli_num_rows($res);
                 $wrong = $total_question - $correct;
                 echo "<br>"; echo "<br>";
@@ -123,7 +123,7 @@ include_once "connection.php";
 if (isset($_SESSION["quiz_start"])){
     date_default_timezone_set('Asia/Jakarta'); // untuk mendapatkan timezone Jakarta
     $date = date("Y-m-d h:i:s"); // untuk mendapatkan tanggal hari ini menggunakan format (Tahun-Bulan-Tanggal)
-    mysqli_query($link, "insert into quiz_results(id_result, username, quiz_name, total_question, correct_answer, wrong_answer, quiz_time) values(NULL,'$_SESSION[username]', '$_SESSION[quiz]', '$total_question', '$correct', '$wrong', '$date')") or die (mysqli_error($link));
+    mysqli_query($link, "INSERT INTO quiz_results values(NULL,'$_SESSION[id_participant]', '$_SESSION[id_quiz]', '$total_question', '$correct', '$wrong', '$date')") or die (mysqli_error($link));
 }
 
 if($isset($_SESSION["quiz_start"])){
